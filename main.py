57428fbc42e556f6ef4e14ea90d8ef4030bb615c
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-09-17 14:28:09 -0300

    feat: rename file as mp3

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pytube import YouTube
+import os
 
 
 def Download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_audio_only()
     try:
-        youtubeObject.download(output_path='./download')
+        output_file = youtubeObject.download(output_path='./download')
+        base, ext = os.path.splitext(output_file)
+        os.rename(output_file, base + '.mp3')
     except:
         print("An error has occurred")
     print("Download is completed successfully")
